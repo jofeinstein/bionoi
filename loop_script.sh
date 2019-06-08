bd93a69e6907551cfea39aa10021286a8dfed48e
jofeinstein <jofeinstein@gmail.com>
2019-06-08 11:58:01 -0500

    changed to apply to files in bionoi directory

--- loop_script.sh
+++ loop_script.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-ls testfiles/popsa |awk -F. '{print $1}'> name.lst
+ls protein_files/bae-pops |awk -F. '{print $1}'> name.lst
 
 for x in `cat name.lst`
 do
 	profile=$x.profile
 	popsa=$x.out
-	mol=$(ls mol2/|grep  "$x"|awk '{print $1}')
+	mol=$(ls ./protein_files/bae-data-mol2/|grep  "$x"|awk '{print $1}')
 	mol2=$(echo $mol|awk '{print $1}')
 	mkdir -p output/$x
-        python3 main.py -profile ./testfiles/profile/$profile -mol ./testfiles/mol2/$mol2 -popsa ./testfiles/popsa/$popsa -out ./output/$x -colorby properties -alpha 0.8 -size 256 -dpi 256
+	python3 main.py -mol ./protein_files/bae-data-mol2/$mol2 -pop ./protein_files/bae-pops/$popsa -profile ./protein_files/bae-profile/$profile -out ./output/$x -alpha 0.8 -size 256 -dpi 256 -colorby properties
 done
 
 rm name.lst
